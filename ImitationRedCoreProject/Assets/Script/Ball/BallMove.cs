7572dfdda0858f01dc60e9fc20c49bf190ba10ac
anyue <1183767577@qq.com>
2021-02-04 10:21:18 +0800

    创建必要文件夹与场景。

---
+++ ImitationRedCoreProject/Assets/Script/Ball/BallMove.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * @Author: l hy 
+ * @Date: 2021-02-04 10:20:04 
+ * @Description: 球体移动逻辑
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
