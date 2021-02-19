4890ac08af54432cf4f59d45dce915157773b361
anyue <1183767577@qq.com>
2021-02-19 20:00:50 +0800

    添加列表

---
+++ ImitationRedCoreProject/Assets/Script/MapEditor/ItemList.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * @Author: l hy 
+ * @Date: 2021-02-19 16:06:22 
+ * @Description: item列表
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemList : MonoBehaviour {
+
+    [Header ("item预制")]
+    public GameObject itemPrefab;
+
+    
+}
