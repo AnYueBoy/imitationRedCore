9a03683f5abc086782950bafaa106eeea0b62fdf
anyue <1183767577@qq.com>
2021-02-20 10:09:37 +0800

    导入模型资源。

---
+++ ImitationRedCoreProject/Assets/Script/Role/Enemy.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * @Author: l hy 
+ * @Date: 2021-02-20 09:38:40 
+ * @Description: 敌人
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+}
